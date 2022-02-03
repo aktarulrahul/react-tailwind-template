9824ae2dd1e56515d360a24acc7cda2e85dddf6a
aktarulrahul <contact.aktarul@gmail.com>
2022-02-03 17:34:59 +0600

    fixing app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
-
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello React & Tailwind!
-    </h1>
+    <div className="bg-cyan-500 m-10 px-8 py-3 text-center border border-cyan-500 rounded-full shadow shadow-cyan-500">
+      <h1 className="text-3xl text-white">React & Tailwind!</h1>
+    </div>
   );
 }
 
